9786c3e0051e21902d419a933cfeb6858c5b50ca
userstar713 <userstar713@gmail.com>
2019-06-14 02:32:06 +0400

    notes module finished

--- libs/core/dynamo_db.py
+++ libs/core/dynamo_db.py
@@ -1,4 +1,5 @@
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 dynamodb = boto3.resource('dynamodb')
 
 
@@ -15,7 +16,45 @@ class BaseModel(object):
             self.table = dynamodb[self.table_name]
 
     @classmethod
-    def list(cls, **kwargs):
+    def get_table(cls):
         if cls.table_name is None:
             raise NotImplementedError('Missing table_name')
-        return dynamodb.Table(cls.table_name).scan()
+        return dynamodb.Table(cls.table_name)
+
+    @classmethod
+    def list(cls, userId, **kwargs):
+        KeyConditionExpression = kwargs.get('KeyConditionExpression', None)
+        if KeyConditionExpression is None:
+            KeyConditionExpression = Key('userId').eq(userId)
+        else:
+            KeyConditionExpression = \
+                KeyConditionExpression & Key('userId').eq(userId)
+        table = cls.get_table()
+        return table.query(
+            KeyConditionExpression=KeyConditionExpression, **kwargs)
+
+    @classmethod
+    def create(cls, item):
+        table = cls.get_table()
+        return table.put_item(Item=item)
+
+    @classmethod
+    def get(cls, param):
+        table = cls.get_table()
+        return table.get_item(Key=param)['Item']
+
+    @classmethod
+    def update(
+            cls, Key, UpdateExpression, ExpressionAttributeValues={},
+            ReturnValues='UPDATED_NEW', **kwargs):
+        table = cls.get_table()
+
+        return table.update_item(
+            Key=Key, UpdateExpression=UpdateExpression,
+            ExpressionAttributeValues=ExpressionAttributeValues,
+            ReturnValues=ReturnValues, **kwargs)
+
+    @classmethod
+    def delete(cls, Key, **kwargs):
+        table = cls.get_table()
+        return table.delete_item(Key=Key, **kwargs)
--- src/notes/notes.py
+++ src/notes/notes.py
@@ -1,8 +1,72 @@
+import json
+import uuid
+from datetime import datetime
 from libs.models.note import Note
 from libs.core.response import return_success, return_failure
 
 
 def index(event, context):
-    result = Note.list()
+    userId = event['requestContext']['identity']['cognitoIdentityId']
+    result = Note.list(userId)
 
     return return_success(result['Items'])
+
+
+def create(event, context):
+    data = json.loads(event['body'])
+    data['userId'] = event['requestContext']['identity']['cognitoIdentityId']
+    data['noteId'] = str(uuid.uuid1())
+    data['created'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    try:
+        response = Note.create(data)
+        return return_success(data)
+    except Exception as e:
+        return return_failure(str(e))
+
+
+def get(event, context):
+    key = {
+        'userId': event['requestContext']['identity']['cognitoIdentityId'],
+        'noteId': event['pathParameters']['id']
+    }
+
+    try:
+        response = Note.get(key)
+        return return_success(response)
+    except Exception as e:
+        return return_failure(str(e))
+
+
+def update(event, context):
+    key = {
+        'userId': event['requestContext']['identity']['cognitoIdentityId'],
+        'noteId': event['pathParameters']['id']
+    }
+
+    data = json.loads(event['body'])
+
+    try:
+        response = Note.update(
+            key,
+            "SET content = :content, attachment = :attachment",
+            {
+                ":attachment": data.get('attachment'),
+                ":content": data.get('content')
+            },
+            ReturnValues="ALL_NEW")
+        return return_success({'status': True})
+    except Exception as e:
+        return return_failure(str(e))
+
+
+def delete(event, context):
+    key = {
+        'userId': event['requestContext']['identity']['cognitoIdentityId'],
+        'noteId': event['pathParameters']['id']
+    }
+
+    try:
+        response = Note.delete(key)
+        return return_success({'status': True})
+    except Exception as e:
+        return return_failure(str(e))
